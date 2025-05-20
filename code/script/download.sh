65191ef32a2233a376da8fb524561889c816bcc8
TingchenFu <1607228797@qq.com>
2025-05-20 11:27:20 +0800

    update download

---
+++ code/script/download.sh
@@ -0,0 +1,7 @@
+for source in gsm8k math500 minerva olympiad aime
+do
+    for constraint in single double triple
+    do
+        wget https://huggingface.co/datasets/TingchenFu/MathIF/resolve/main/${source}_${constraint}.jsonl -o data/${source}_${constraint}.jsonl
+    done
+done
