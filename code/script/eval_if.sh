adc37f0c83cbbff168230ae97f4d5b26dc09192e
TzWng <91708166+TzWng@users.noreply.github.com>
2025-10-19 20:00:21 -0400

    Update model name in eval_if.sh script

--- code/script/eval_if.sh
+++ code/script/eval_if.sh
@@ -1,4 +1,4 @@
-model=deepseek-ai_DeepSeek-R1-Distill-Qwen-1.5B
+model=tzwilliam0/dpo_Instruct_merged
 for dataset in gsm8k math500 minerva olympiad aime
 do
     for constraint in single double triple 
