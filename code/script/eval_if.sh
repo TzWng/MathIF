101b8d36058db004b6df48b1cbdf94b3e46c58c0
TzWng <91708166+TzWng@users.noreply.github.com>
2025-10-22 04:48:20 -0400

    Update eval_if.sh

--- code/script/eval_if.sh
+++ code/script/eval_if.sh
@@ -23,9 +23,9 @@ model=tzwilliam0_dpo_Instruct_merged
 model=Qwen_Qwen2.5-Math-1.5B
 model=Qwen_Qwen2.5-1.5B-Instruct
 model=Qwen_Qwen2.5-Math-1.5B-Instruct
-model=tzwilliam0_DPO_deepseek_Instruct
+# model=tzwilliam0_DPO_deepseek_Instruct
 # model=tzwilliam0_DPO_deepseek_Math
-
+model=tzwilliam0_DPO_Qwen_Math_Instruct
 
 strict_total=0
 loose_total=0
